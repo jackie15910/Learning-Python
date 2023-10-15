42d0a94f77c10726cbdc59b016160ef29dac1ac8
Jackie <jackie15910@gmail.com>
2023-10-15 16:13:58 -0400

    Initial Commit

---
+++ Slice.py
@@ -0,0 +1,11 @@
+name = "Jackie Luc"
+
+first_name = name[0:6]
+last_name = name[7:10]
+reversed_name = name[::-1]
+# first_name = name[:6]
+# last_name = name[7:]
+
+print(first_name)
+print(last_name)
+print(reversed_name)
---
+++ helloWorld.py
@@ -0,0 +1,15 @@
+print("Hello World")
+
+hello_world = "Hello World"
+
+print(hello_world)
+
+print(type(hello_world))
+
+name = input("What is your name?: ")
+age = input("How old are you?: ")
+
+age = int(age) + 1
+
+print("Hello "+name)
+print("You are turning "+str(age)+" years old")
